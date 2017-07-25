b6ae6b530af8c6a4394b059633e7532055de2caa
Shrinivassab <shrinivassab@gmail.com>
2017-07-24 20:08:02 -0400

    Before deployement

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit,Input, HostBinding } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article: Article;
+
+  constructor() {
+    // this.article = new Article('Angular 2', 'http://angular.io', 10);
+    // this.article = new Article('Full Stack', 'http://fullstack.io', 3);
+    // this.article = new Article('Angular HomePage', 'http://angular.io', 3);
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
